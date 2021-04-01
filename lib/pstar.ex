d0f41d6c215fa3cb00a1b88277496afd1fc413a8
Matheus T. dos Santos <tenoriomatheus0@gmail.com>
2021-04-01 19:13:24 -0300

    Initial setup
    Signed-off-by: Matheus T. dos Santos <tenoriomatheus0@gmail.com>

---
+++ lib/pstar.ex
@@ -0,0 +1,18 @@
+defmodule Pstar do
+  @moduledoc """
+  Documentation for `Pstar`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pstar.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pstar_test.exs
@@ -0,0 +1,8 @@
+defmodule PstarTest do
+  use ExUnit.Case
+  doctest Pstar
+
+  test "greets the world" do
+    assert Pstar.hello() == :world
+  end
+end

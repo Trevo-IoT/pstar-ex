1c5a657c1d026755b5e548b7a67196b2ba536712
Matheus T. dos Santos <tenoriomatheus0@gmail.com>
2021-04-06 19:36:22 -0300

    Doing integration test
    Signed-off-by: Matheus T. dos Santos <tenoriomatheus0@gmail.com>

--- lib/p_star.ex
+++ lib/p_star.ex
@@ -1,18 +1,87 @@
 defmodule PStar do
+  require Logger
   @moduledoc """
   Documentation for `PStar`.
   """
 
+  @port 0x5053
+
   @doc """
-  Hello world.
+  Open port 0x5053 to receive protocol requisitions.
+
+  Returns: `:ok`.
 
   ## Examples
 
-      iex> PStar.hello()
-      :world
+      iex> PStar.start
+      :ok
 
   """
-  def hello do
-    :world
+  @doc since: "0.1.0"
+  def start do
+    {:ok, socket} = :gen_tcp.listen(@port, [:binary, :inet, active: false, header: 3, keepalive: true, packet: :raw])
+    Logger.info("Accepting connections on port #{@port}")
+    accept_loop(socket)
+  end
+
+  @doc """
+  Creates new star with name `star`.
+
+  Returns: `{:ok, PID} or {:err, reason}`
+
+  ## Examples
+
+      iex> PStar.open("hello")
+      {:ok, PID}
+      iex> PStar.open("hello")
+      {:err, :exist}
+  """
+  def open(star) do
+    # add star to constellation
+
+    # new center task
+  end
+
+  def close(star) do
+    #check if is in constellation
+
+    # kill center task
+    # kill edge task associated with star
+    # remove from constelation
+  end
+
+  def broadcast(star, content) do
+    # check
+  end
+
+  defp accept_loop(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    Logger.info("New connection accepted")
+    Task.start_link(fn -> handle_incoming(client) end)
+    accept_loop(socket)
+  end
+
+
+
+  def handle_incoming(socket) do
+    {:ok, raw_data} = socket |> :gen_tcp.recv(0)
+
+    {:ok, decoded_data} = raw_data |> PStar.Message.decode
+
+    :ok = decoded_data |> handle
+
+    handle_incoming(socket)
+  end
+
+  defp handle(%PStar.Message{type: :connect, star: _star}) do
+    :ok
+  end
+
+  defp handle(%PStar.Message{type: :disconnect, star: _star}) do
+    :ok
+  end
+
+  defp handle(%PStar.Message{type: :data, star: _star, content: _content}) do
+    :ok
   end
 end
--- test/p_star_test.exs
+++ test/p_star_test.exs
@@ -2,7 +2,19 @@ defmodule PStarTest do
   use ExUnit.Case
   doctest PStar
 
-  test "greets the world" do
-    assert PStar.hello() == :world
+  @addr {127, 0, 0, 1}
+  @port 0x5053
+
+  test "client echo" do
+    data_out = "\x01\x04\x0btestHello World"
+
+    Task.start_link(fn -> PStar.start end)
+
+    {:ok, client} = :gen_tcp.connect(@addr, @port, [:binary, :inet, active: false, keepalive: true, packet: :raw])
+    client |> :gen_tcp.send(data_out)
+    {:ok, data_in} = client |> :gen_tcp.recv(0)
+    client |> :gen_tcp.close
+
+    assert data_in == data_out
   end
 end
